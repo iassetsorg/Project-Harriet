6435b6529940a8903dd099d90840caff401a2eb4
iassetsorg <mail@iAssets.org>
2024-01-23 04:35:19 +0400

    update 404

--- src/components/404.tsx
+++ src/components/404.tsx
@@ -5,18 +5,18 @@ import { Link } from "react-router-dom";
 
 const NotFoundPage: React.FC = () => {
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-100">
-      <div className="text-center">
-        <h1 className="text-6xl font-bold text-gray-800">404</h1>
-        <p className="text-lg text-gray-600">
+    <div className=" text-center  w-full bg-gray-800 0">
+      <div className="text-center mx-auto mt-16">
+        <h1 className="text-6xl font-bold text-gray-300">404</h1>
+        <p className="text-lg text-gray-300">
           Oops! The page you're looking for isn't here.
         </p>
-        <Link
-          to="/"
-          className="mt-6 inline-block px-6 py-2 text-sm font-medium leading-5 text-center text-white uppercase transition-colors duration-200 transform bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:bg-blue-500"
+        <a
+          href="/Explore"
+          className="inline-flex items-center px-8 py-4 mt-5 text-xl font-semibold text-gray-800 bg-indigo-300 rounded-full hover:bg-indigo-400 transition duration-300"
         >
-          Go back home
-        </Link>
+          Explore
+        </a>
       </div>
     </div>
   );
