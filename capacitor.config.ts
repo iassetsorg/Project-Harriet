c47d7c6aa6fd7e5cc83cb36488356adda812d49b
iassetsorg <mail@iAssets.org>
2024-12-08 00:49:02 +0400

    Android and IOS app via capacitorjs

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ibird.app',
+  appName: 'ibird',
+  webDir: 'build'
+};
+
+export default config;
